a5ab9403301158dc6421ef71fed3d1aa82e10dca
Anton Lysenko <anton.lys96@gmail.com>
2021-08-27 22:16:15 +0300

    fixedServletConfig

--- src/main/java/edu/lysenko/catalog/config/AppInit.java
+++ src/main/java/edu/lysenko/catalog/config/AppInit.java
@@ -1,24 +1,24 @@
 package edu.lysenko.catalog.config;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import edu.lysenko.catalog.config.AppInit;
 
-public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class AppInit implements WebApplicationInitializer {
 
 	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return null;
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(WebConfig.class);
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("SpringDispatcher",
+				new DispatcherServlet(context));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
 	}
-
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[] { WebConfig.class };
-	}
-
-	@Override
-	protected String[] getServletMappings() {
-		return new String[] { "/" };
-	}
-
 }
